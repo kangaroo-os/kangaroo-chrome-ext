ac0c7735bfd1b3c64433a03c1a3363c0704cb693
kozr <zxzflow@gmail.com>
2022-05-23 23:49:55 -0700

    remove useless

--- background.js
+++ background.js
@@ -1,19 +1,19 @@
-const BASE_URL = "http://localhost:3000";
-const CHROME_PDF_VIEWER = "chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai";
-const CHROME_PDF_VIEWER_NO_PROTOCOL = "mhjfbmdgcfjbbpaeojofohoefgiehjai";
+const BASE_URL = 'http://localhost:3000'
+const CHROME_PDF_VIEWER = 'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai'
+const CHROME_PDF_VIEWER_NO_PROTOCOL = 'mhjfbmdgcfjbbpaeojofohoefgiehjai'
 
 const uploadFileToKangaroo = async (name, url, type) => {
-  const downloadResponse = await fetch(url);
-  const blob = await downloadResponse.blob();
-  const file = new File([blob], name, { type: type });
-  const formData = new FormData();
-  formData.append("file", file);
-  const postResponse = await fetch(`${BASE_URL}/cloud_files/upload`, {
+  const downloadResponse = await fetch(url)
+  const blob = await downloadResponse.blob()
+  const file = new File([blob], name, { type: type })
+  const formData = new FormData()
+  formData.append('file', file)
+  await fetch(`${BASE_URL}/cloud_files/upload`, {
     headers: await getAuthHeader(),
-    method: "POST",
+    method: 'POST',
     body: formData,
-  });
-};
+  })
+}
 
 // Prevents the file dialogue from showing
 // Automatically downloads the file to Kangaroo
@@ -23,134 +23,147 @@ chrome.downloads.onDeterminingFilename.addListener((e) => {
 })
 
 const fetchSessionTokenFromKangaroo = () => {
-  return localStorage.getItem("user");
-};
+  return localStorage.getItem('user')
+}
 
 const fetchSessionToken = async () => {
-  const tab = await getKangarooTab();
+  const tab = await getKangarooTab()
   try {
     if (tab) {
       const [scriptResponse] = await chrome.scripting.executeScript({
         target: { tabId: tab.id },
         function: fetchSessionTokenFromKangaroo,
-      });
-      const { accessToken, client, email, tokenExpiresAt } = JSON.parse(
-        scriptResponse.result
-      );
+      })
+      const { accessToken, client, email, tokenExpiresAt } = JSON.parse(scriptResponse.result)
       await chrome.storage.local.set({
         auth_header: {
-          "access-token": accessToken,
+          'access-token': accessToken,
           client: client,
           expiry: tokenExpiresAt,
           uid: email,
-          "token-type": "Bearer",
+          'token-type': 'Bearer',
         },
-      });
-      return true;
+      })
+      return true
     }
   } catch (e) {
-    console.log(e || "Ran into an error finding the access token");
-    return false;
+    console.log(e || 'Ran into an error finding the access token')
+    return false
   }
-};
+}
 
 const getKangarooTab = async () => {
-  const tabs = await chrome.tabs.query({});
+  const tabs = await chrome.tabs.query({})
   for (let i = 0; i < tabs.length; i++) {
-    const url = tabs[i].url;
+    const url = tabs[i].url
     if (url?.startsWith(BASE_URL)) {
-      return tabs[i];
+      return tabs[i]
     }
   }
-  console.error('Could not find Kangaroo tab');
-  return null;
-};
-
-const fetchAuth = async () => {
-  return fetchSessionToken();
-};
+  console.error('Could not find Kangaroo tab')
+  return null
+}
 
 const getFilesFromS3 = async () => {
   const res = await fetch(`${BASE_URL}/cloud_files`, {
     headers: await getAuthHeader(),
-  });
-  const { files } = await res.json();
-  return files;
-};
+  })
+  const { files } = await res.json()
+  return files
+}
 
 const getAuthHeader = async () => {
-  const storage = await chrome.storage.local.get("auth_header");
-  return storage.auth_header;
-};
+  const storage = await chrome.storage.local.get('auth_header')
+  return storage.auth_header
+}
 
 const retrieveFileFromS3 = async (fileId) => {
   const res = await fetch(`${BASE_URL}/cloud_files/${fileId}`, {
     headers: await getAuthHeader(),
-  });
-  return res.json();
-};
+  })
+  return res.json()
+}
 
 const uploadLinkToKangaroo = async (url) => {
   await fetch(`${BASE_URL}/link_files/upload`, {
     headers: {
       ...(await getAuthHeader()),
-      "Accept": "application/json",
-      "Content-Type": "application/json",
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
     },
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify({
       path: url,
     }),
-  });
-};
+  })
+}
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.event === "cloud-file-detected") {
-    (async () => {
-      if (await fetchAuth()) {
-        const { fileId } = message;
-        const file = await retrieveFileFromS3(fileId);
-        sendResponse({
-          status: "ok",
-          url: file.url,
-          name: file.name,
-          fileType: file.file_type,
-        });
-      }
-    })();
-  } else if (message.event === "upload-to-kangaroo-from-url") {
-    (async () => {
-      if (await fetchAuth()) {
-        let { name, url, type } = message;
-        if (!name) {
-          const [tab] = await chrome.tabs.query({
-            active: true,
-            currentWindow: true,
-          });
-          name = tab.title;
+  if (message.event === 'cloud-file-detected') {
+    ;(async () => {
+      if (await fetchSessionToken()) {
+        try {
+          const { fileId } = message
+          const file = await retrieveFileFromS3(fileId)
+          sendResponse({
+            status: 'ok',
+            url: file.url,
+            name: file.name,
+            fileType: file.file_type,
+          })
+        } catch (e) {
+          sendResponse({
+            status: 'error',
+            error: e,
+          })
         }
-        uploadFileToKangaroo(name, url, type);
-        sendResponse({ status: "ok" });
       }
-    })();
-  } else if (message.event === "get-file-list") {
-    (async () => {
-      if (await fetchAuth()) {
-        const files = await getFilesFromS3();
-        sendResponse({ status: "ok", files: files });
+    })()
+  } else if (message.event === 'upload-to-kangaroo-from-url') {
+    ;(async () => {
+      if (await fetchSessionToken()) {
+        try {
+          let { name, url, type } = message
+          if (!name) {
+            const [tab] = await chrome.tabs.query({
+              active: true,
+              currentWindow: true,
+            })
+            name = tab.title
+          }
+          uploadFileToKangaroo(name, url, type)
+          sendResponse({ status: 'ok' })
+        } catch (e) {
+          sendResponse({ status: 'error', error: e })
+        }
       }
-    })();
-  } else if (message.event === "save-current-website") {
-    (async () => {
-      if (await fetchAuth()) {
-        await uploadLinkToKangaroo(message.url);
-        sendResponse({ status: "ok" });
+    })()
+  } else if (message.event === 'get-file-list') {
+    ;(async () => {
+      if (await fetchSessionToken()) {
+        try {
+          const files = await getFilesFromS3()
+          sendResponse({ status: 'ok', files: files })
+        } catch (e) {
+          sendResponse({ status: 'error', error: e })
+        }
       }
-    })();
+    })()
+  } else if (message.event === 'save-current-website') {
+    ;(async () => {
+      if (await fetchSessionToken()) {
+        try {
+          await uploadLinkToKangaroo(message.url)
+          sendResponse({ status: 'ok' })
+        } catch (e) {
+          sendResponse({ status: 'error', error: e })
+        }
+      }
+    })()
   }
-  return true;
-});
+  return true
+})
 
 function getUserData() {
-  return localStorage.getItem("user_data");
+  return localStorage.getItem('user_data')
 }
